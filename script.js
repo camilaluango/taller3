ed3cbb5e6f515ac4a0cb7a7c55b54d0a345a894a
camila <mariacamilaluango@gmail.com>
2024-11-05 13:27:55 -0500

    resultado 1

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 let nombre = prompt ("ingrese su nombre")
-let eCivil = parseInt(prompt("¿cual es su estado civil? : soltero = 1 , casado = 2, separado = 3, viudo = 4, union libre = 6 "))
+let eCivil = parseInt(prompt("¿cual es su estado civil? : soltero = 1 , casado = 2, separado = 3, viudo = 4, union libre = 5 "))
 
 switch (eCivil){
     case 1: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
+    document.write("señor@ : ", nombre , " su estado civil es: solter@");
     break;
-    case 2: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
+    case 2:  
+    document.write("señor@ : ", nombre , " su estado civil es: casad@");
     break;
     case 3: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
+    document.write("señor@ : ", nombre , " su estado civil es: separad@");
     break;
     case 4: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
+    document.write("señor@ : ", nombre , " su estado civil es: viud@");
     break;
     case 5: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
-    break;
-    case 6: 
-    document.write("señor@", nombre , "su estado civil es: solter@");
+    document.write("señor@ : ", nombre , " su estado civil es: union libre");
     break;
     default:
         document.write ("error")
